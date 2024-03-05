e07c9f5197c3ee921f39d9d003918080ad2307c6
elena-seneff <144173269+elena-seneff@users.noreply.github.com>
2024-03-05 13:47:46 -0800

    3/5/24
    Started code

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager gm;
-    public List<Card> deck = new List<Card>();
+    public List<Card> deck = new List<Card>(52);
     public List<Card> player_deck = new List<Card>();
     public List<Card> ai_deck = new List<Card>();
     public List<Card> player_hand = new List<Card>();
     public List<Card> ai_hand = new List<Card>();
     public List<Card> discard_pile = new List<Card>();
+
+
+    public Card trout;
+    public Card GiantSquid;
+    public Card Jellyfish;
+
+
+    private float whatCard;
+
 
     private void Awake()
     {
@@ -24,21 +33,33 @@ public class GameManager : MonoBehaviour
             DontDestroyOnLoad(this.gameObject);
         }
     }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        deck.Add(trout);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        whatCard = Random.Range(1, 2);
     }
 
     void Deal()
     {
 
+        if (whatCard == 1)
+        {
+            player_deck.Add(trout);
+            deck.Remove(trout);
+        }
+
+        if (whatCard == 2)
+        {
+            player_deck.Add(GiantSquid);
+            deck.Remove(GiantSquid);
+        }
     }
 
     void Shuffle()
